d475d1ed5f04634285286493a89134b8cf7af212
middlemine <38240895+middlemine@users.noreply.github.com>
2018-04-26 07:37:06 +0700

    Ok

--- quit_wampserver.bat
+++
@@ -1,5 +0,0 @@
-echo off
-NET STOP wampapache
-NET STOP wampmysqld
-NET STOP wampmariadb
-start /wait c:\wamp\wampmanager.exe -quit -id={wampserver}
--- uninstall_services.bat
+++
@@ -1,7 +0,0 @@
-start /wait c:\wamp\wampmanager.exe -quit -id={wampserver}
-NET STOP wampapache
-sc delete wampapache
-NET STOP wampmysqld
-sc delete wampmysqld
-NET STOP wampmariadb
-sc delete wampmariadb
